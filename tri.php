abfc6592b2586fb1fec52a72582fd8ed0b63b0c0
bennypurbaa <34009729+bennypurbaa@users.noreply.github.com>
2020-04-26 02:20:38 +0700

    Add files via upload

---
+++ tri.php
@@ -0,0 +1,23 @@
+bingung do au 
+boha do dalan ni parhaletan on
+
+hu haholongi ho ito
+hape nunga adong donganki
+
+sai hu pikkiri,
+dang targotap au
+ 
+sada sian na dua 
+na laho mandongani au
+
+reff
+
+sugari boi haholongi
+hamu nadua di rohakki
+
+ulahonon hu i hasian
+nangpe umbalga holongmi tu au
+sian gallet hi 
+
+nangpe ikkon adong na kecewa
+tinggal ma ho ito lului ma na asing i 
